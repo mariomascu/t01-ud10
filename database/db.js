afd63209aa5d040533a4a19a67da73f65dce007f
benjiserrano <bjserranoweb@gmail.com>
2025-05-23 11:22:55 +0200

    Fix .env

--- database/db.js
+++ database/db.js
@@ -1,14 +1,14 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Para leer variables de .env
+dotenv.config();
 
 const connection = await mysql.createConnection({
-  host: "localhost",   
-  user: "root",   
-  password: "", 
-  database: "empresadb", // Cambia esto por el nombre de tu base de datos
-  port: 3306     
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 export default connection;
